fb7e616aca1548577bbb537a9be8caf9cd4d5b1e
SCPassion <141584825+SCPassion@users.noreply.github.com>
2025-03-01 17:48:43 +0100

    modified content

--- js/index.js
+++ js/index.js
@@ -4,6 +4,22 @@ const queries = priceFeedId.map((priceFeed) => `ids[]=${priceFeed.id}`).join('&'
 const url = `${baseURL}${queries}`
 
 const ctx = document.getElementById('myChart').getContext('2d');
+const form = document.getElementsByClassName('form')[0]
+const dropDown = document.getElementsByClassName('crypto-dropdown')[0]
+
+let selectedCrypto
+
+priceFeedId.forEach((priceFeed)=> {
+    dropDown.innerHTML += `<option value="${priceFeed.sym}">${priceFeed.sym.toUpperCase()}</option>`
+})
+
+form.addEventListener('submit', (e)=> {
+    e.preventDefault()
+    selectedCrypto = dropDown.value
+    myLineChart.data.datasets[0].label = `${selectedCrypto.toUpperCase()}`
+    myLineChart.data.datasets[0].data = []
+    myLineChart.data.labels = []
+})
 
 // Create a new Chart instance
 const myLineChart = new Chart(ctx, {
@@ -11,7 +27,7 @@ const myLineChart = new Chart(ctx, {
     data: {
         labels: [],
         datasets: [{
-            label: 'Monthly Sales',
+            label: 'BTC',
             data: [], // Sample data points
             borderColor: 'rgba(75, 192, 192, 1)', // Line color
             backgroundColor: 'rgba(75, 192, 192, 0.3)', // Fill under the line
@@ -44,12 +60,23 @@ const myLineChart = new Chart(ctx, {
                     }
                 }
             }
+        },
+        plugins: {
+            legend: {
+                display: true,
+                labels: {
+                    color: 'whitesmoke',
+                    font: {
+                        size: 40
+                    }
+                }
+            }
         }
     }
 });
 
 function updateChartValue(publishTime, price) {
-    if(myLineChart.data.labels.length >= 10) {
+    if(myLineChart.data.labels.length >= 100) {
         myLineChart.data.labels.shift();  // Remove first label
         myLineChart.data.datasets[0].data.shift()
     }
@@ -63,7 +90,14 @@ function updateChartValue(publishTime, price) {
 
 const priceFeedFetcher = setInterval(async ()=> {
     const priceFeeds  = await fetchPythPrice(url)
-    updateChartValue(priceFeeds[1].publishTime, priceFeeds[1].price)
+    if(selectedCrypto) {
+        const selectedPriceFeed = priceFeeds.filter((priceFeed)=> priceFeed.name === selectedCrypto)[0]
+        updateChartValue(selectedPriceFeed.publishTime, selectedPriceFeed.price)
+
+    } else {
+        // default to use BTC priceFeed
+        updateChartValue(priceFeeds[0].publishTime, priceFeeds[0].price)
+    }
 }, 500)
 
 // setTimeout(()=> clearInterval(priceFeedFetcher), 10000)
