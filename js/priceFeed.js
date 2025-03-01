d6c468ac40cd0a2328a41b886ac87c71319abb43
SCPassion <141584825+SCPassion@users.noreply.github.com>
2025-03-01 21:43:53 +0100

    added akt and sol

--- js/priceFeed.js
+++ js/priceFeed.js
@@ -4,6 +4,10 @@ export const priceFeedId = [
     },
     {   sym: 'eth',
         id: 'ff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace'
+    }, 
+    {
+        sym: 'sol',
+        id: 'ef0d8b6fda2ceba41da15d4095d1da392a0d2f8ed0c6c7bc0f4cfac8c280b56d'
     },
     {   sym: 'atom',
         id: 'b00b60f88b03a6a625a8d1c048c3f66653edf217439983d037e7222c4e612819'
@@ -13,6 +17,10 @@ export const priceFeedId = [
     },
     {   sym: 'tia',
         id: '09f7c1d7dfbb7df2b8fe3d3d87ee94a2259d212da4f30c1f0540d066dfa44723'
+    }, 
+    {
+        sym: 'akt',
+        id: '4ea5bb4d2f5900cc2e97ba534240950740b4d3b89fe712a94a7304fd2fd92702'
     }
 ]
 
